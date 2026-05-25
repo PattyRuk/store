057ddffd423fdca97dd90d714d26f04f8d589da3
weiyu <yin1532776643@gmail.com>
2026-05-25 12:37:33 -0500

    fotter build

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer>
+    <p>© 2026 My First Store | All Rights Reserved</p>
+    <p>Contact: example@gmial.com</p>
+    </footer>
+  );
+}
