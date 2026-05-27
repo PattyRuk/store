ac1603d7ed1c9640348b42ec19733d6b3d46164f
patrick rukundo <prukundo@mail.com>
2026-05-27 15:36:01 -0500

    added routing to cart

---
+++ src/pages/Cart.js
@@ -0,0 +1,15 @@
+import PageWrapper from '../components/PageWrapper';
+
+const Cart = ({}) => {
+
+  return (
+    <PageWrapper>
+      <div className="cart-container">
+        <h1>Shopping Cart</h1>
+
+      </div>
+    </PageWrapper>
+  );
+};
+
+export default Cart;
