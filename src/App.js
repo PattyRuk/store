56e9613ac1e7510e77120c606e504e37e2bb6980
patrick rukundo <prukundo@mail.com>
2026-05-27 12:26:29 -0500

    adding product details page

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
       <main>
         <Banner />
         <SecondBanner />
+        <ProductDetail />
       </main>
       <Footer />
     </>
---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import PageWrapper from '../components/PageWrapper';
+import SecondBanner from '../components/SecondBanner';
+import ProductCard from '../components/ProductCard';
+
+const ProductDetail = ({ addToCart }) => {
+
+
+  return (
+    <PageWrapper>
+
+    </PageWrapper>
+  );
+};
+
+export default ProductDetail; 
