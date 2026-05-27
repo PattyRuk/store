1368f3703964e1bc601452da81d62b30b3fcda78
patrick rukundo <prukundo@mail.com>
2026-05-27 11:17:52 -0500

    second banner addition

---
+++ src/components/SecondBanner.js
@@ -0,0 +1,10 @@
+import { FaShippingFast, FaUndoAlt, FaShieldAlt, FaHeadset } from 'react-icons/fa';
+import '../styles/banner.css';
+
+export default function SecondBanner() {
+  return (
+    <div className="second-banner">
+
+    </div>
+  );
+}
