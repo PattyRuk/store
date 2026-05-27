c9f9709015e59763bc663fda9c7d747782ace940
patrick rukundo <prukundo@mail.com>
2026-05-27 11:18:54 -0500

    first perk card added

--- src/components/SecondBanner.js
+++ src/components/SecondBanner.js
@@ -4,6 +4,13 @@ import '../styles/banner.css';
 export default function SecondBanner() {
   return (
     <div className="second-banner">
+        <div className="perk-card">
+            <FaShippingFast className="perk-icon" />
+            <div>
+            <h4>Express Delivery</h4>
+            <p>Dispatched globally within 24 hours</p>
+            </div>
+        </div>
 
     </div>
   );
